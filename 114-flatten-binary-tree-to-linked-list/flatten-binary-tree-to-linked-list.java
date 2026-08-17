dac5254815d93f78376e9ba7a74d376864c3400b
devsharma0007 <dev716415@gmail.com>
2026-08-18 01:45:54 +0530

    Time: 1 ms (19.15%) | Memory: 44 MB (34.44%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -14,11 +14,19 @@
  * }
  */
 class Solution {
+    public void preorder(ArrayList<TreeNode> arr,TreeNode root){
+        if(root==null) return;
+        arr.add(root);
+        preorder(arr,root.left);
+        preorder(arr,root.right);
+    }
     public void flatten(TreeNode root) {
         if(root==null) return;
         ArrayList<TreeNode> arr = new ArrayList<>();
-        preorder(root,arr);
-        for (int i = 0; i < arr.size() - 1; i++) {
+
+        preorder(arr,root);
+
+        for(int i = 0;i<arr.size()-1;i++){
             TreeNode curr = arr.get(i);
             TreeNode next = arr.get(i + 1);
 
@@ -26,18 +34,8 @@ class Solution {
             curr.right = next;
         }
 
-        // Last node
         TreeNode last = arr.get(arr.size() - 1);
         last.left = null;
         last.right = null;
-        
-        
     }
-    public void preorder(TreeNode root,ArrayList<TreeNode> arr){
-        if(root==null) return;
-        arr.add(root);
-        preorder(root.left,arr);
-        preorder(root.right,arr);
-    }
-
 }
